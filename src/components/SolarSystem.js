5451c7b324560b460f8512aa73162ba4e1376192
Carolina Pedrosa <carolinasouza0@gmail.com>
2023-05-04 15:23:18 -0300

    feat: requisito 6

--- src/components/SolarSystem.js
+++ src/components/SolarSystem.js
@@ -1,11 +1,23 @@
 import { Component } from 'react';
 import Title from './Title';
+import Planets from '../data/planets';
+import PlanetCard from './PlanetCard';
 
 class SolarSystem extends Component {
   render() {
     return (
       <div data-testid="solar-system">
         <Title headline="Planetas" />
+        {
+          Planets
+            .map(({ name, image }) => (
+              <PlanetCard
+                key={ name }
+                planetName={ name }
+                planetImage={ image }
+              />
+            ))
+        }
       </div>
     );
   }
